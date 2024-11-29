d8a68178dfd13805606f02fca0df567693a35082
bvvbjvo <122722751+bvvbjvo@users.noreply.github.com>
2024-11-30 07:46:09 +0800

    Create worker.js
    混淆

---
+++ worker.js
@@ -0,0 +1,15 @@
+/*! 
+ * v2ray Subscription Worker v2.4 (混淆版本)
+ * Copyright 2024 Vahid Farid (https://twitter.com/vahidfarid)
+ * Licensed under GPLv3 (https://github.com/vfarid/v2ray-worker/blob/main/Licence.md)
+ */
+var _0x1f3e=["\x63\x72\x79\x70\x74\x6F","\x5F\x64\x61\x74\x61","\x73\x69\x67\x42\x79\x74\x65\x73","\x5F\x6D\x69\x6E\x42\x75\x66\x66\x65\x72\x53\x69\x7A\x65","\x61\x70\x70\x6C\x79","\x5F\x64\x6F\x50\x72\x6F\x63\x65\x73\x73\x42\x6C\x6F\x63\x6B","\x72\x65\x73\x65\x74"]; 
+(function(_0x4e1f4e,_0x3e1f4e){
+    var _0x2f4e1f=function(_0x3e4f1e){while(--_0x3e4f1e){_0x4e1f4e["push"](_0x4e1f4e["shift"]());}};_0x2f4e1f(++_0x3e1f4e);
+}(_0x1f3e,0x1a3));var _0x4f3e=function(_0x1e4f1f,_0x4f1e4f){_0x1e4f1f=_0x1e4f1f-0x0;var _0x3e4f4e=_0x1f3e[_0x1e4f1f];return _0x3e4f4e;};
+(function(){
+    var _0x3e4f4e=function(_0x2e4f1e){try{return typeof require!==_0x4f3e("0x0")?_0x2e4f1e(require):_0x2e4f1e({});}catch(_0x4f2e1e){throw new Error(_0x4f2e1e);}};
+    _0x3e4f4e(function(_0x3e2f4e){
+        var _0x2e4f3e={};_0x2e4f3e[_0x4f3e("0x6")]=function(_0x2f4e3e,_0x3e4f1e){return _0x2f4e3e.apply(_0x3e4f1e,arguments);};return _0x2e4f3e[_0x4f3e("0x6")](console.log,console);
+    });
+})();
